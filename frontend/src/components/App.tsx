fec2b3bae8a07e907a568147ceca32932abe67ae
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-27 01:22:59 +0300

    add classes

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Game } from './Game/Game';
+import css from './App.module.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Guess the Number Game</h1>
+    <div className={css.app}>
+      <h1 className={css.header}>Вгадай Число!</h1>
       <Game />
     </div>
   );
