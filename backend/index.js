2aab5fcaa04cef427ecb52d0d9b3363f9eaa21cf
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-26 22:02:13 +0300

    add app logic

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import router from './routers/number.js';
+import { routeNotFoundHandler } from './middlewares/routeNotFoundHandler.js';
+import { errorHandler } from './middlewares/internalErrorHandler.js';
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
+
+app.use('/api', router);
+
+app.use(routeNotFoundHandler);
+
+app.use(errorHandler);
+
+app.listen(3000, () => console.log('Server running on port 3000'));
