489eb87db8267e5bf63dd501eab7e4b64b0e5872
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-26 22:23:48 +0300

    set project structure

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,11 @@
+import { Game } from './Game/Game';
+
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Game/Game.tsx
@@ -0,0 +1,5 @@
+import css from './Game.module.css';
+
+export const Game = () => {
+  <></>;
+};
