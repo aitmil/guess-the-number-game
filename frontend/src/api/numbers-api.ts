54286eccd20a890d1e141008b7786f91cc69be24
Anna Taranchuk <taranchuk4@yahoo.com>
2024-08-27 15:41:02 +0300

    update api url

--- frontend/src/api/numbers-api.ts
+++ frontend/src/api/numbers-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://guess-the-number-game-129p.onrender.com/api';
 
 export const startGame = async (): Promise<string> => {
   try {
